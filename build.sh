483e5688c1464df9a64e8a7c982ca786ab2f5eb3
arshadbagwan1920 <arshadbagwan1920@users.noreply.github.com>
2026-07-10 14:14:14 +0530

    fix: rm @medusajs/draft-order before build (hard dep of @medusajs/medusa, broken admin bundle)

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
+
 NODE_ENV=development npm install
+
+# @medusajs/medusa lists draft-order as a dep, but its admin bundle is broken
+# Remove it so Vite doesn't try to compile the busted admin widgets
+rm -rf node_modules/@medusajs/draft-order
+
 node node_modules/@medusajs/cli/cli.js build
